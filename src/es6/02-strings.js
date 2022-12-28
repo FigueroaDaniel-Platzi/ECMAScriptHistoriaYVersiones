cb59b6adaecc19f9c6987a11daf26dc83fa78c6e
Daniel Figueroa <da.figuepa@gmail.com>
2022-12-28 18:13:12 -0500

    ES6: strings class5

---
+++ src/es6/02-strings.js
@@ -0,0 +1,16 @@
+// Template Literals
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// Multi-line strings
+let lorem = 'Esto es un string\n' + 'esto es otra linea';
+let lorem2 = `Esta es una frase épica
+la continuación de esta frase épica`
+
+console.log(lorem);
+console.log(lorem2);
