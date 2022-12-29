1294c9c45597157bff901e96f70b1ae6256912b8
Daniel Figueroa <da.figuepa@gmail.com>
2022-12-29 16:58:10 -0500

    ES6: spread operator class8

--- src/es6/03-default-params.js
+++ src/es6/03-default-params.js
@@ -7,3 +7,10 @@ function newUser(name, age, country) {
 
 newUser();
 newUser('Alfredo', 31, 'CA');
+
+function admin(name = 'Daniel', age = 30, country = 'CL') {
+  console.log(name, age, country);
+}
+
+admin();
+admin('Alfredo', 29, 'UK');
--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -9,3 +9,19 @@ console.log(a, fruits[1]);
 let user = {username: 'Daniel', age:34};
 let {username, age} = user;
 console.log(username, user.age);
+
+// spread operator
+let person = {name: 'Daniel', age: 30};
+let country = 'CL';
+
+let data = {id: 1, ...person, country};
+console.log(data);
+
+// rest
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3);
