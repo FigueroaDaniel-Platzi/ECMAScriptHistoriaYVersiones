61eada9aa2cdc4b5c4bfd6733d546e0a3b7d5049
Daniel Figueroa <da.figuepa@gmail.com>
2022-12-30 17:51:15 -0500

    ES8: object entries y object values class19

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,7 @@
+let numbers = [1, 2, 4, 5, 6, 7, 8];
+console.log(numbers.includes(4));
+console.log(numbers.includes(9));
+
+const list = ['oscar', 'daniel', 'angelica'];
+console.log(list.includes('daniel'));
+console.log(list.includes('Daniel'));
